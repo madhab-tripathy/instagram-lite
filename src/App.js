c1ca6804db24c495e61ee8e81068ed427cc423ce
madhab-tripathy <madhabtripathy1349@gmail.com>
2023-08-26 02:14:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from './Components/Signup'
+
+const App = (()=>{
+    return (
+        <div>
+            <Signup />
+        </div>
+    )
+})
+
+export default App
---
+++ src/Components/Signup.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import {Typography,Card} from "@mui/material"
+
+const Signup = (()=>{   
+    return(
+        <div>
+            
+        </div>
+    )
+});
+
+export default Signup;
